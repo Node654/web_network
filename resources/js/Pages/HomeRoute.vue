a96ee18b870b175add1e87854507b20b5869f589
nodest <amtes927@gmail.com>
2024-08-01 17:19:53 +0300

    create web network project

---
+++ resources/js/Pages/HomeRoute.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>Home</h1>
+        <router-link to="/test">Перейди на тест page</router-link>
+    </div>
+
+    <div>
+        <h1 class="text-3xl font-bold">
+            Hello world!
+        </h1>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/TestRoute.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>Test page!</h1>
+        <router-link to="/">Home</router-link>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
